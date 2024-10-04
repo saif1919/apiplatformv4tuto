e16f5dbfc5e4ee55acba29968c82803a00190592
SaifEddineNaimi <dragonnoir1919@gmail.com>
2024-10-04 20:15:10 +0200

    first commit

---
+++ src/Entity/Article.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
+use App\Repository\ArticleRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ArticleRepository::class)]
+#[Get()]
+#[GetCollection(
+    // paginationEnabled: false
+    // paginationItemsPerPage: 20,
+    paginationClientEnabled: true,
+    paginationClientItemsPerPage: true,
+    uriTemplate: '/getarticle',
+    name: 'getarticle'
+)]
+#[GetCollection(
+    uriTemplate: '/getarticle2',
+    name: 'getarticle2'
+)]
+#[GetCollection()]
+#[Post()]
+#[Put()]
+#[Patch()]
+#[Delete()]
+class Article
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $content = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $publishedAt = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): static
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getPublishedAt(): ?\DateTimeImmutable
+    {
+        return $this->publishedAt;
+    }
+
+    public function setPublishedAt(\DateTimeImmutable $publishedAt): static
+    {
+        $this->publishedAt = $publishedAt;
+
+        return $this;
+    }
+}
